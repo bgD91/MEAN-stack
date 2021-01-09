e66397f2b92e3392f86fa27399ba0999b5cd6ec1
bgD91 <32795242+bgD91@users.noreply.github.com>
2021-01-09 13:55:50 +0200

    Implemented proper client and server routing, full CRUD operations on messages, other quality of life improvements

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,24 +1,52 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
-import { PostsService } from '../posts.service';
+import {PostsService} from '../posts.service';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {Post} from '../post.model';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
 
-  constructor(public postsService: PostsService) {}
+  private componentState = 'create';
+  private postId: string;
+  public post: Post;
+
+  constructor(public postsService: PostsService, private route: ActivatedRoute) {
+  }
+
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.componentState = 'edit';
+        this.postId = paramMap.get('postId');
+        this.postsService.getPost(this.postId).subscribe((postData) => {
+          this.post = {id: postData.post._id, title: postData.post.title, content: postData.post.content};
+        });
+      } else {
+        this.componentState = 'create';
+        this.postId = null;
+      }
+    });
+  }
 
   onAddPost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    console.log(this.route);
+    if (this.componentState === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.editPost(this.postId, form.value.title, form.value.content);
+    }
     form.resetForm();
   }
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -4,6 +4,7 @@ import {Subject} from 'rxjs';
 import {Post} from './post.model';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 
 @Injectable({providedIn: 'root'})
@@ -11,7 +12,11 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private router: Router) {
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
   }
 
   getPosts() {
@@ -31,8 +36,8 @@ export class PostsService {
       });
   }
 
-  getPostUpdateListener() {
-    return this.postsUpdated.asObservable();
+  getPost(id: string) {
+    return this.http.get<{ post: { _id: string, title: string, content: string } }>(`http://localhost:3456/api/posts/${id}`);
   }
 
   addPost(title: string, content: string) {
@@ -50,7 +55,22 @@ export class PostsService {
       .subscribe((response) => {
         const updatedPosts = this.posts.filter(post => post.id !== id);
         this.posts = updatedPosts;
-        this.postsUpdated.next(updatedPosts);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  editPost(id: string, title: string, content: string) {
+    const post: Post = {id, title, content};
+    return this.http.put<{ message: string }>(`http://localhost:3456/api/posts/${id}`, post)
+      .subscribe((response) => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next(this.posts);
+        this.router.navigate(['/']).then(() => {
+          console.log('back to homepage');
+        });
       });
   }
 }
