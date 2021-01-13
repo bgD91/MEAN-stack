a1f6fa4ac22ddc3462e8dc267638aeea32c04333
bgD91 <32795242+bgD91@users.noreply.github.com>
2021-01-13 11:21:00 +0200

    Backend optimizations: sepparated middleware, cleaned up routes, added controllers

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,78 +1,16 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const User = require('../models/user');
 
+const UserController = require("../controllers/user");
 
 const router = express.Router();
 
 router.post(
   "/register",
-  (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-          email: req.body.email,
-          password: hash
-        });
-        user.save()
-          .then(result => {
-            res.status(201).json({
-              message: 'User created',
-              result: result
-            })
-          })
-          .catch(err => {
-            res.status(500).json({
-              message: "Invalid authentication credentials!"
-            });
-          })
-      });
-  });
+  UserController.createUser);
 
 router.post(
   "/login",
-  (req, res, next) => {
-    let fetchedUser;
-    User.findOne({email: req.body.email})
-      .then(user => {
-        if (!user) {
-          return res.status(404).json({
-            message: 'Invalid user',
-            request: req,
-          });
-        }
-        fetchedUser = user;
-        console.log(fetchedUser);
-        return bcrypt.compare(req.body.password, user.password);
-      })
-      .then(result => {
-        console.log(result);
-        if (!result) {
-          return res.status(401), json({
-            message: 'Invalid password'
-          });
-        }
-        const token = jwt.sign({
-            email: fetchedUser.email,
-            userId: fetchedUser._id
-          },
-          'very_long_secret_for_development12312',
-          {expiresIn: '1h'});
-        res.status(200).json({
-          token: token,
-          expiresIn: 3600,
-          userId: fetchedUser._id
-        });
-      })
-      .catch(authError => {
-        return res.status(401).json({
-          message: 'Invalid authentication credentials!',
-          error: authError
-        })
-      })
+  UserController.loginUser);
 
-  }
-);
 
 module.exports = router;
